cff5c456bb9305cfff5cf6bcefe96c867b8a2cd7
schaefereric <schaefereric@pm.me>
2024-01-15 17:21:07 +0100

    Projektdateien hinzufügen.

---
+++ palindrome_checker.cpp
@@ -0,0 +1,48 @@
+// palindrome_checker.cpp 
+
+#include <iostream>
+#include <string>
+
+
+int main()
+{   
+    std::string input;
+    std::cout << "Enter word: ";
+    std::cin >> input;
+
+    std::string reversal;
+
+    // reverse string
+    for (int i = input.size() - 1; i >= 0; i--) {
+        reversal.push_back(input[i]);
+    }
+
+    // print input and reversal
+    std::cout << std::endl << input << " - " << reversal << std::endl;
+    //std::cout << "input size:" << input.size() << "  reversal size: " << reversal.size() << std::endl;
+
+    // determine difference
+    int similarity_count = 0;
+    for (int i = 0; i < input.size(); i++) {
+        if (input[i] == reversal[i]) {
+            similarity_count++;
+        }
+    }
+
+    // show results
+    if (similarity_count == input.size()) {
+        std::cout << "It's a palindrome!";
+    }
+    else if (similarity_count >= input.size() - 3) {
+        std::cout << "Almost a palindrome!";
+    }
+    else {
+        std::cout << "Not a palindrome!";
+    }
+
+    std::cout << std::endl;
+    std::cout << "Similarity Count: " << similarity_count << std::endl;
+
+    return 0;
+}
+
